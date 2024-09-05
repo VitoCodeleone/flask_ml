05950001e2417448c8b7c6fe0b8d19e3c905c8e1
kp <kskpisthebest@gmail.com>
2024-09-05 17:14:18 +0530

    Backend and templates

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import (
+    Flask,
+    url_for,
+    render_template
+)
+from forms import InputForm
+import pandas as pd
+import joblib
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret_key"
+
+model = joblib.load("model/model.joblib")
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html", title="Home")
+
+@app.route("/predict", methods=["GET", "POST"])
+def predict():
+    form = InputForm()
+    if form.validate_on_submit():
+        X_input = pd.DataFrame(dict(
+            airline=[form.airline.data],
+            date_of_journey=[form.date_of_journey.data.strftime("%Y-%m-%d")],
+            source=[form.source.data],
+            destination=[form.destination.data],
+            dep_time=[form.dep_time.data.strftime("%H:%M:%S")],
+            arrival_time=[form.arrival_time.data.strftime("%H:%M:%S")],
+            duration=[form.duration.data],
+            total_stops=[form.total_stops.data],
+            additional_info=[form.additional_info.data]
+        ))
+
+        prediction = model.predict(X_input)[0]
+        message = f"The predicted price is {prediction:.2f}"
+    else:
+        message = "Please provide valid input details"
+    return render_template("predict.html", title="Predict", form=form, output=message)
+
+if __name__ == "__main__":
+    app.run(debug=True)
